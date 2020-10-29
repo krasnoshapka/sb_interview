98379dd0474ee1c13b428e0cc5309deea3359f2f
Volodymyr Krasnoshapka <shlapa@gmail.com>
2020-10-29 17:53:00 +0200

    Implemented pagination.

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,31 +1,44 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import axios from 'axios';
 import MovieCard from "./MovieCard";
 import {API_KEY, API_URL} from "../utils/conf";
+import {Link} from "react-router-dom";
 
 function MovieList() {
   const [query, setQuery] = useState('');
   const [loading, setLoading] = useState(false);
-  const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
+  const [movies, setMovies] = useState(null);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Fetch movies
+  const fetchMovies = () => {
     setLoading(true);
     axios
       .get(API_URL, {
         params: {
           apikey: API_KEY,
-          s: query
+          s: query,
+          page: page
         }
       })
       .then((response) => {
-        setMovies(response.data.Search);
+        const {Search, totalResults, Response} = response.data;
+        if (Response == 'True') {
+          setMovies({Search, totalResults});
+        }
         setLoading(false);
       })
       .catch((error) => {
         console.log(error);
       });
+  }
+
+  useEffect(() => {
+    fetchMovies();
+  }, [page]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchMovies();
   }
 
   const handleChange = (event) => {
@@ -39,12 +52,16 @@ function MovieList() {
     </form>
     {
       loading ? (<span>Loading...</span>) : (
-        <div id='movies'>
-          {movies.length > 0 ? movies.map((movie) => (
-              <MovieCard movie={movie} />
+        <div>
+          <div id='movies'>
+            {movies && movies.Search.length > 0 && movies.Search.map((movie) => (
+                <MovieCard movie={movie} />
+              )
             )
-          ) : ''
-          }
+            }
+          </div>
+          {movies && page > 1 && (<Link to='/' onClick={() => {setPage(page-1)}} >Previous page </Link>)}
+          {movies && (page + 1) <= Math.ceil(movies.totalResults / 10) && (<Link to='/' onClick={() => {setPage(page+1)}} > Next page</Link>)}
         </div>
       )
     }
