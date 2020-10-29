f1c79a7734d568d87f13118d2e6c58be1af07548
Volodymyr Krasnoshapka <shlapa@gmail.com>
2020-10-29 15:20:46 +0200

    Created react app.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Movie catalogue
+      </header>
+    </div>
+  );
+}
+
+export default App;
